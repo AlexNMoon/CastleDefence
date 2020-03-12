b5ba51ff24d5ff937fc1ddfc59baeab81bcf153b
Alexandra <alexandramoon5@gmail.com>
2020-03-12 21:50:58 +0200

    feat (scriptable objects): create scriptable object scripts for tower and enemy

---
+++ Assets/Scripts/ScriptableObjects/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu (fileName = "Enemy", menuName = "Game/Enemy")]
+    public class Enemy : ScriptableObject
+    {
+        public float Health;
+        public float Speed;
+        public float Damage;
+        public EnemyController Prefab;
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Tower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu (fileName = "Tower", menuName = "Game/Tower")]
+    public class Tower : ScriptableObject
+    {
+        public int BuildPrice;
+        public float Range;
+        public Sprite Image;
+        public float ShootInterval;
+        public float Damage;
+    }
+}
